b87bc076c2356123073836684cf448ec672e545b
Robin Hagmayer <robin.hagmayer@proton.me>
2024-09-18 20:09:44 +0200

    Added comments to the Graph and Edge struct

--- src/graph/graph.h
+++ src/graph/graph.h
@@ -3,18 +3,29 @@
 #include <ostream>
 #include <vector>
 
+/**
+ * @brief Represents an edge of a weighted graph.
+ *
+ * The Edge struct contains the source id, the target id and the weight of an
+ * edge.
+ * (uint32_t src_id, uint32_t trg_id, uint32_t weight)
+ * It also overloads the "<<" operator for a better output to stdout.
+ */
 struct Edge {
   uint32_t src_id;
   uint32_t trg_id;
   uint32_t weight;
 
-  /*Edge(int s, int t, int w) : src_id(s), trg_id(t), weight(w) {}*/
-
-  /*Edge() : src_id(0), trg_id(0), weight(0) {}*/
-
   friend std::ostream &operator<<(std::ostream &os, const Edge &edge);
 };
 
+/**
+ * @brief Represents a directed and weighted graph.
+ *
+ * The Graph struct contains a sorted vector of edges and an offset vector
+ * which stores the first edge index for all vertices.
+ * (std::vector<uint32_t> node_offsets, std::vector<Edge> edges)
+ */
 struct Graph {
   std::vector<uint32_t> node_offsets;
   std::vector<Edge> edges;
